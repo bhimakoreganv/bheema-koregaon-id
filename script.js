0f347f3053478d3255211a980af7b401a4a96d52
bhimakoreganv <bhimakoreganv@gmail.com>
2026-07-19 02:05:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.querySelectorAll("button")[0].onclick = function () {
+    alert("Member Registration Coming Soon!");
+};
+
+document.querySelectorAll("button")[1].onclick = function () {
+    alert("Member ID Verification Coming Soon!");
+};
+
+document.querySelectorAll("button")[2].onclick = function () {
+    alert("Contact: bheemakoregaon@example.com");
+};
